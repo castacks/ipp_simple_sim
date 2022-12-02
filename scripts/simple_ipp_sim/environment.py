2c53380d17aaf393193b10359444c7aab266d154
Nayana Suvarna <nls5260@gmail.com>
2022-12-02 00:43:00 -0500

    changes to allow for simulating multiple agents from separate ipp nodes

--- scripts/simple_ipp_sim/environment.py
+++ scripts/simple_ipp_sim/environment.py
@@ -6,13 +6,14 @@ from simple_ipp_sim.agent import *
 from simple_ipp_sim.target import *
 from simple_ipp_sim.sensor import *
 from geographic_msgs.msg import GeoPose
+from planner_map_interfaces.msg import Plan
 
 
 class Environment:
     def __init__(self, list_of_target_dicts=[], max_omega=5, max_zvel=5,
                  init_x=None, init_y=None, init_z=None, init_psi=None,
                  K_p=0.01, K_p_z=0.01,
-                 agent_l=3, hvel=5, vvel=2, n_rand_targets=-1, del_t=0.02,
+                 num_agents=1, agent_l=3, hvel=5, vvel=2, n_rand_targets=-1, del_t=0.02,
                  waypoint_threshold=5,
                  sensor_focal_length=5, sensor_width=10, sensor_height=10, sensor_pitch=20, sensor_max_range=500,
                  sensor_endurance=5, sensor_hedge=0):
@@ -34,6 +35,7 @@ class Environment:
 
         self.del_t = del_t
 
+        self.num_agents = num_agents #number of agents to simulate
         self.agent_l = agent_l  # agent length
         self.hvel = hvel  # horizontal velocity
         self.vvel = vvel  # vertical velocity
@@ -48,32 +50,27 @@ class Environment:
         self.sensor_endurance = sensor_endurance
         self.sensor_hedge = sensor_hedge
 
+        self.K_p = K_p  # x-y proportionality constant for PID controller
+        self.K_p_z = K_p_z  # z-axis proportionality constant for PID controller
+        self.waypoint_threshold = waypoint_threshold
+
         # if targets not specified, randomly generate between 1-10 targets
         self.n_rand_targets = random.randrange(1, 10) if not list_of_target_dicts and n_rand_targets == -1 else n_rand_targets
 
         self.targets = self.generate_targets(list_of_target_dicts, self.n_rand_targets)
 
-        self.global_waypoint_list = []
+        self.global_waypoint_list = [Plan() for i in range(self.num_agents)] # self.global_waypoint_list = []
+        self.agent = [self.init_agent(i) for i in range(num_agents)]  # self.agent = self.init_agent()
+        self.prev_agentxyz = [[0.0, 0.0, 0.0] for i in range(self.num_agents)] # self.prev_agentxyz = [0.0, 0.0, 0.0]
+        self.curr_waypoint_num = [0 for i in range(num_agents)]  # self.curr_waypoint_num = 0
+        self.remaining_budget = [0 for i in range(num_agents)] # self.remaining_budget = 0
 
-        self.waypoint_threshold = waypoint_threshold
-
-        self.K_p = K_p  # x-y proportionality constant for PID controller
-        self.K_p_z = K_p_z  # z-axis proportionality constant for PID controller
-
-        self.agent = self.init_agent()
-        self.prev_agentxyz = [0.0, 0.0, 0.0]
         self.sensor = self.init_sensor()
-
-        self.curr_waypoint_num = 0
-        self.remaining_budget = 0
-
-        self.prev_time = -1
 
     def generate_targets(self, list_of_target_dicts, n_rand_targets=None):
         '''
         Generates ships with initial positions
         '''
-
         # when no targets specified
         if not list_of_target_dicts:
             if n_rand_targets is None:
@@ -106,14 +103,15 @@ class Environment:
             ]
         return targets
 
-    def init_agent(self):
-        return Agent(init_x=self.init_x,
-                       init_y=self.init_y,
-                       init_z=self.init_z,
-                       init_psi=self.init_psi,
-                       agent_l=self.agent_l,
-                       hvel=self.hvel,
-                       vvel=self.vvel)
+    def init_agent(self, agent_num):
+        return Agent(agent_num = agent_num,
+                     init_x=self.init_x,
+                     init_y=self.init_y+(agent_num*200),
+                     init_z=self.init_z+(agent_num*20),
+                     init_psi=self.init_psi,
+                     agent_l=self.agent_l,
+                     hvel=self.hvel,
+                     vvel=self.vvel)
 
     def init_sensor(self):
         return SensorModel(
@@ -130,88 +128,78 @@ class Environment:
         '''
         Get sensor measurements from camera sensor
         '''
-        camera_projection = self.get_ground_intersect(
-            [self.agent.x, self.agent.y, self.agent.z], self.sensor_pitch,
-            self.agent.psi)
-        detected_targets = []
-        for target in self.targets:
-            if self.sensor.is_point_inside_camera_projection([target.x, target.y],
+        detected_targets_list = []
+        camera_projections_list = []
+        for i in range(self.num_agents):
+            camera_projection = self.get_ground_intersect(
+            [self.agent[i].x, self.agent[i].y, self.agent[i].z], self.sensor_pitch,
+            self.agent[i].psi)
+            detected_targets = []
+            for target in self.targets:
+                if self.sensor.is_point_inside_camera_projection([target.x, target.y],
                                                              camera_projection):
-                range_to_target = np.linalg.norm(
+                    range_to_target = np.linalg.norm(
                     np.array([target.x, target.y, 0]) - np.array(
-                        [self.agent.x, self.agent.y, self.agent.z]))
-                # is_detected = self.sensor.get_detection(range_to_target)
-                detection_prob = np.random.random()
-                sensor_tpr = self.sensor.tpr(range_to_target)
-                if detection_prob < sensor_tpr:
-                    target.is_detected = True
-                    detected_targets.append(target)
-        return detected_targets, camera_projection
+                        [self.agent[i].x, self.agent[i].y, self.agent[i].z]))
+                    # is_detected = self.sensor.get_detection(range_to_target)
+                    detection_prob = np.random.random()
+                    sensor_tpr = self.sensor.tpr(range_to_target)
+                    if detection_prob < sensor_tpr:
+                        target.is_detected = True
+                        detected_targets.append(target)
+            detected_targets_list.append(detected_targets)
+            camera_projections_list.append(camera_projection)
+        return detected_targets_list, camera_projections_list
 
-    def traverse(self):
+    def traverse(self, delta_t):
         '''
         Waypoint manager and agent state update- moves agent towards waypoints as long as waypoints exist in global_waypoint_list
         '''
-        if not self.global_waypoint_list or len(self.global_waypoint_list.plan) == 0:
-            return
-        else:
-            next_position = np.array(
-                [self.global_waypoint_list.plan[0].position.position.x,
-                 self.global_waypoint_list.plan[0].position.position.y,
-                 self.global_waypoint_list.plan[0].position.position.z])
-            dist_to_waypoint = np.linalg.norm(
-                [self.agent.x, self.agent.y, self.agent.z] - next_position)
-
-            # update waypoint list if reached waypoint
-            if dist_to_waypoint < self.waypoint_threshold:
-                # print("Reached waypoint -> ", next_position)
-                self.curr_waypoint_num += 1
-                self.global_waypoint_list.plan.pop(0)
-
-
-            omega, z_d = self.agent.go_to_goal(self.max_omega, self.max_zvel,
+        for i in range(self.num_agents):
+            agent_waypoint_list = self.global_waypoint_list[i]
+            if len(agent_waypoint_list.plan) == 0:
+                continue
+            else:
+                next_position = np.array(
+                    [agent_waypoint_list.plan[0].position.position.x,
+                    agent_waypoint_list.plan[0].position.position.y,
+                    agent_waypoint_list.plan[0].position.position.z])
+                dist_to_waypoint = np.linalg.norm(
+                    [self.agent[i].x, self.agent[i].y, self.agent[i].z] - next_position)
+                # update waypoint list if reached waypoint
+                if dist_to_waypoint < self.waypoint_threshold:
+                    self.curr_waypoint_num[i] += 1
+                    agent_waypoint_list.plan.pop(0)
+                omega, z_d = self.agent[i].go_to_goal(self.max_omega, self.max_zvel,
                                                     next_position, self.K_p,
                                                     self.K_p_z)
-            curr_time  = rospy.get_time()
-            delta_t = self.del_t
-            if self.prev_time == -1:
-                self.prev_time = rospy.get_time()
-            else:
-                delta_t = curr_time - self.prev_time
-            # if abs(delta_t - self.del_t) > 0.005:
-            #     print("WARNING! AGENT JUMPED TIME")
-            #     print("delta_t: ", delta_t)
-            #     print("del_t: ", self.del_t)
-            self.agent.psi += delta_t* omega
-            self.agent.x += delta_t * self.hvel * math.cos(self.agent.psi)
-            self.agent.y += delta_t * self.hvel * math.sin(self.agent.psi)
-            self.agent.z += delta_t * z_d
+                self.agent[i].psi += delta_t * omega
+                self.agent[i].x += delta_t * self.hvel * math.cos(self.agent[i].psi)
+                self.agent[i].y += delta_t * self.hvel * math.sin(self.agent[i].psi)
+                self.agent[i].z += delta_t * z_d
+                delta_dist = np.linalg.norm(
+                        np.array([self.agent[i].x, self.agent[i].y, self.agent[i].z]) - np.array(self.prev_agentxyz[i]))
+                self.remaining_budget[i] -= delta_dist
+                self.prev_agentxyz[i] = [self.agent[i].x, self.agent[i].y, self.agent[i].z]
 
-            delta_dist = np.linalg.norm(np.array([self.agent.x, self.agent.y, self.agent.z]) - np.array(self.prev_agentxyz))
-            self.remaining_budget -= delta_dist
-            self.prev_time = curr_time
-            self.prev_agentxyz = [self.agent.x, self.agent.y, self.agent.z]
-
-    def update_waypoints(self, new_wpts):
+    def update_waypoints(self, new_wpts, id_num):
         '''
         Receive new waypoints and send them to waypoint manager
         '''
-        # self.global_waypoint_list.append(new_wpts)
-        self.global_waypoint_list = new_wpts
-        self.curr_waypoint_num = 0
+        self.global_waypoint_list[id_num] = new_wpts
+        self.curr_waypoint_num[id_num] = 0
 
     def update_states(self):
         '''
         Updates the environment states
         '''
-        self.traverse()
-
+        self.traverse(self.del_t)
         # update the states for all ships in the environment
         for target in self.targets:
             target.propagate(self.del_t)
 
-    def get_agent_uncertainty(self):
-        return self.agent.position_uncertainty()
+    def get_agent_uncertainty(self,id_num):
+        return self.agent[id_num].position_uncertainty()
 
     # function that gets target heading and return heading with gaussian noise
     def get_target_heading_noise(self, heading):
