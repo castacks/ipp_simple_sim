99a81d9f9a254c8780063af5111a92ee33d901cf
tshaik32 <83743580+tshaik32@users.noreply.github.com>
2023-12-14 19:01:23 -0500

    Implemented a kalman filter for uncertainty in user class

--- scripts/ipp_simple_sim/agent.py
+++ scripts/ipp_simple_sim/agent.py
@@ -1,6 +1,31 @@
 import math
 import numpy as np
 
+class KalmanFilter:
+    '''
+    Kalman filter for the agent
+    '''
+    def __init__(self, initial_state, initial_estimate_error, process_noise, measurement_noise):
+        self.state_estimate = initial_state
+        self.estimate_error = initial_estimate_error
+        self.process_noise = process_noise
+        self.measurement_noise = measurement_noise
+
+    def predict(self, control_input):
+        # Prediction step
+        # Simulate agent dynamics in the prediction step
+        self.state_estimate += control_input
+        self.estimate_error += self.process_noise
+
+    def update(self, measurement):
+        # Update step
+        kalman_gain = self.estimate_error / (self.estimate_error + self.measurement_noise)
+        self.state_estimate += kalman_gain * (measurement - self.state_estimate)
+        self.estimate_error *= (1 - kalman_gain)
+
+    def get_state_estimate(self):
+        return self.state_estimate
+    
 
 class Agent:
     '''
@@ -17,6 +42,14 @@ class Agent:
         self.vel = hvel
         self.vvel = vvel
 
+        # Kalman filter parameters
+        initial_state = np.array([init_x, init_y, init_z])
+        initial_estimate_error = np.array([0.1, 0.1, 0.1])  # Initial position uncertainty
+        process_noise = np.array([0.01, 0.01, 0.01])  # Process noise
+        measurement_noise = np.array([0.1, 0.1, 0.1])  # Measurement noise
+
+        self.kalman_filter = KalmanFilter(initial_state, initial_estimate_error, process_noise, measurement_noise)
+
     def go_to_goal(self, max_omega, max_zvel, next_waypoint, K_p, K_p_z):
         '''
         Returns angular velocity and velocity in z-axis towards desired direction
@@ -32,6 +65,19 @@ class Agent:
             omega = max_omega  # setting max angular vel
         if z_d > max_zvel:
             z_d = max_zvel
+
+        # Update Kalman filter with measurement
+        measurement = np.array([self.x, self.y, self.z]) + np.random.normal(0, self.kalman_filter.measurement_noise)
+        self.kalman_filter.update(measurement)
+
+        # Predict next state based on control input
+        control_input = np.array([omega, self.vel, z_d])
+        self.kalman_filter.predict(control_input)
+
+        # Get the estimated state from Kalman filter
+        estimated_state = self.kalman_filter.get_state_estimate()
+        self.x, self.y, self.z = estimated_state
+
         return omega, z_d
 
     def position_uncertainty(self):
@@ -46,4 +92,5 @@ class Agent:
         sigma_roll = 0.1
         sigma_pitch = 0.1
         sigma_yaw = 0.1
-        return [sigma_x, sigma_y, sigma_z, sigma_roll, sigma_pitch, sigma_yaw]
+        naive_estimate = [sigma_x, sigma_y, sigma_z, sigma_roll, sigma_pitch, sigma_yaw]
+        return self.kalman_filter.estimate_error.tolist()
